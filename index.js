44027a7223991babd005afc90a8b38b7aaf93648
toonuaaa <149750175+toonuaaa@users.noreply.github.com>
2024-04-17 21:12:03 +0800

    add new endpoint

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const print = require("./controller");
 
-app.get('/', print);
+app.get("/", print);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+app.get("/introduce", (req, res) => {
+  res.send("Hello I am Lin Xuan");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.PORT}`);
+});
